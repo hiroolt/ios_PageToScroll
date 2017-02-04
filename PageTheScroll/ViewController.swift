2302ce331ef398c3ea58050dd55be3daaf1dcd75
hiroolt <sunasuna.8@gmail.com>
2017-02-04 22:22:45 +0200

    Initial Commit

---
+++ PageTheScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Hiroki Sunagawa on 04/02/2017.
+//  Copyright © 2017 Hiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
